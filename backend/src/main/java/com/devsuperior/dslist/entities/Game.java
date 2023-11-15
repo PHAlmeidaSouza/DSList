d9ba7327013e289c0a3dc70336af88e88ae36078
Pedro Henrique <ph8584774@gmail.com>
2023-11-15 00:54:13 -0300

    Game repository, service e controller

--- backend/src/main/java/com/devsuperior/dslist/entities/Game.java
+++ backend/src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -2,11 +2,16 @@ package com.devsuperior.dslist.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_game")
-public class Game {
+public class Game implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
